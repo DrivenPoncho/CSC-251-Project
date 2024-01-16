8b5cce6aafb6574d57d07a13dc12fb90a140c4ec
DrivenPoncho <144631184+DrivenPoncho@users.noreply.github.com>
2024-01-15 19:10:35 -0500

    Add files via upload
    New and improved policy program!

--- Project_Mika_Ohin.java
+++ Project_Mika_Ohin.java
@@ -1,75 +1,42 @@
 import java.util.Scanner;
- 
- 
- public class Project_Mika_Ohin
- {
-   public static void main (String[] args)
+import java.util.*;
+
+public class Project_Mika_Ohin
+{
+public static void main(String[]args)
    {
-      Policy policyProgram = new Policy();
-      
-      int inPolyNum;
-      String inPolyName;
-      String inPolyFirst;
-      String inPolyLast;
-      int inPolyAge;
-      String inPolySmoke;
-      double inPolyHeight;
-      double inPolyWeight;
-      double BMI;
-      double price;
-      
-      int smokerCheck = 0;//self-explanatory.
-      
       Scanner scan = new Scanner(System.in);
       
-      System.out.println("Please enter the Policy #: ");
+      int pNum = 0;
+      String provider = "";
+      String fName = "";
+      String lName = "";
+      int age = 0;
+      String smoking = "";
+      double height = 0;
+      double weight = 0;
       
-      inPolyNum = scan.nextInt();
-               
+      System.out.println("Please enter the Policy Number: ");
+      pNum = scan.nextInt();
       System.out.println("Please enter the Provider Name: ");
       scan.nextLine();
-
-      inPolyName = scan.nextLine();
-         
-      System.out.println("Please enter the Policyholder's First Name:");
-      
-      inPolyFirst = scan.nextLine();
-      
-      
-      System.out.println("Please enter the Policyholder's Last Name:");
-      
-      inPolyLast = scan.nextLine();
-            
-      System.out.println("Please enter the Policyholder's Age: ");
-      
-      inPolyAge = scan.nextInt();
-      
-      System.out.println("Please enter the Policyholder's smoking status (smoker/non-smoker): ");
+      provider = scan.nextLine();
+      System.out.println("Please enter the Policyholder’s First Name: ");
+      fName = scan.nextLine();
+      System.out.println("Please enter the Policyholder’s Last Name: ");
+      lName = scan.nextLine();
+      System.out.println("Please enter the Policyholder’s Age: ");
+      age = scan.nextInt();
+      System.out.println("Please enter the Policyholder’s Smoking Status (smoker/non-smoker): ");
       scan.nextLine();
-      inPolySmoke = scan.nextLine();
-      if (inPolySmoke == "smoker" || inPolySmoke == "Smoker")
-      {
-         smokerCheck = smokerCheck + 1;
-      }
-      if (inPolySmoke == "nonsmoker" || inPolySmoke == "non-smoker" || inPolySmoke == "Non-Smoker")
-      {
-         smokerCheck = smokerCheck + 2;
-      }
+      smoking = scan.nextLine();
+      System.out.println("Please enter the Policyholder’s Height (in inches): ");
+      height = scan.nextDouble();
+      System.out.println("Please enter the Policyholder’s Weight (in pounds): ");
+      weight = scan.nextDouble();
       
-      System.out.println("Please enter the Policyholder's Height (In Inches): ");
+      Policy policy = new Policy(pNum, provider, fName, lName, age, smoking, height, weight);
       
-      inPolyHeight = scan.nextInt();
-      
-      System.out.println("Please enter the Policyholder's Weight (In Pounds)");
-      
-      inPolyWeight = scan.nextDouble();
-      
-      
-      BMI = policyProgram.BMI(inPolyHeight, inPolyWeight);
-      
-      price = policyProgram.price(inPolyAge, smokerCheck, BMI);
-      
-      policyProgram.results(inPolyNum, inPolyName, inPolyFirst, inPolyLast, inPolyAge, inPolySmoke, inPolyHeight, inPolyWeight, BMI, price);   
-      
+      System.out.println("Policy Number: " + pNum + "\nProvider Name : " + provider + "\nPolicyholder’s First Name: " + fName + "\nPolicyholder’s Last Name: " + lName + "\nPolicyholder’s Age: " + age + "\nPolicyholder’s Smoking Status: " + smoking + "\nPolicyholder’s Height: " + height + "\nPolicyholder’s Weight: " + weight + "\nPolicyholder’s BMI: " + String.format("%.2f", policy.getBMI()) + "\nPolicy Price : $" + String.format("%.2f", policy.getPrice()));
    }
- }
+}
